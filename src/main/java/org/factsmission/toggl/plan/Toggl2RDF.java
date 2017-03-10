440ecc7894ca1a0ac458c31807cefe4d1cfc868e
Reto Gmür <me@farewellutopia.com>
2017-03-10 12:28:59 +0000

    Creating RDF from data retrieved via API

--- src/main/java/org/factsmission/toggl/plan/Toggl2RDF.java
+++ src/main/java/org/factsmission/toggl/plan/Toggl2RDF.java
@@ -10,16 +10,30 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.apache.clerezza.commons.rdf.Graph;
+import org.apache.clerezza.commons.rdf.IRI;
+import org.apache.clerezza.commons.rdf.RDFTerm;
+import org.apache.clerezza.commons.rdf.impl.utils.PlainLiteralImpl;
+import org.apache.clerezza.commons.rdf.impl.utils.TripleImpl;
+import org.apache.clerezza.commons.rdf.impl.utils.simple.SimpleGraph;
+import org.apache.clerezza.rdf.ontologies.RDF;
 
 /**
  *
  * @author user
  */
-public class Main {
+public class Toggl2RDF {
   
+  public static final String iriPrefix ="http://toggl.wymiwyg.com/";
   
-  public Main(String togglApiToken) {
-    JToggl jToggl = new JToggl(togglApiToken, "api_token");
+  public static IRI prefix(String suffix) {
+    return new IRI(iriPrefix+suffix);
+  } 
+  
+  final JToggl jToggl;
+  
+  public Toggl2RDF(String togglApiToken) {
+    jToggl = new JToggl(togglApiToken, "api_token");
     jToggl.setThrottlePeriod(500l);
     //jToggl.switchLoggingOn();
 
@@ -45,9 +59,23 @@ public class Main {
       System.out.println("User: "+user.getFullname());
     }
   }
+  
+  public void copyToGraph(Graph g, Date startDate, Date endDate) {
+    List<Workspace> workspaces = jToggl.getWorkspaces();
+    for (int i = 0; i < workspaces.size(); i++) {
+      Workspace workspace = workspaces.get(i);
+      IRI workspaceNode = new IRI(iriPrefix+"workspace/"+workspace.getId());
+      g.add(new TripleImpl(workspaceNode, RDF.type, prefix("Workspace")));
+      g.add(new TripleImpl(workspaceNode, prefix("name"), new PlainLiteralImpl(workspace.getName())));
+      g.add(new TripleImpl(workspaceNode, prefix("json"), new PlainLiteralImpl(workspace.toJSONString())));
+    }
+  }
 
   public static void main(String... args) {
     String togglApiToken = args[0];
-    new Main(togglApiToken);
+    Toggl2RDF toggl2RDF = new Toggl2RDF(togglApiToken);
+    Graph g = new SimpleGraph();
+    toggl2RDF.copyToGraph(g, new Date(System.currentTimeMillis()-24*60*60*1000), new Date());
+    System.out.println(g);
   }
 }
