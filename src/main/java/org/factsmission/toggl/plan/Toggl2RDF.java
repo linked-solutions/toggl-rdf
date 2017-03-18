b91c1fb815b1a1f28d1a7d6508f45959c58af67d
Reto Gmür <me@farewellutopia.com>
2017-03-18 19:19:40 +0000

    gets essential workspace information and time entries

--- src/main/java/org/factsmission/toggl/plan/Toggl2RDF.java
+++ src/main/java/org/factsmission/toggl/plan/Toggl2RDF.java
@@ -25,19 +25,25 @@ package org.factsmission.toggl.plan;
 
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.Collection;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import static java.util.stream.Collectors.toList;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.Feature;
 import org.apache.clerezza.commons.rdf.Graph;
+import org.apache.clerezza.commons.rdf.IRI;
+import org.apache.clerezza.commons.rdf.Triple;
 import org.apache.clerezza.commons.rdf.impl.utils.simple.SimpleGraph;
 import org.apache.clerezza.rdf.core.serializedform.Serializer;
 import org.apache.clerezza.rdf.core.serializedform.UnsupportedFormatException;
+import org.apache.clerezza.rdf.ontologies.RDF;
+import org.apache.clerezza.rdf.utils.UnionGraph;
 import org.glassfish.jersey.client.ClientConfig;
-import org.glassfish.jersey.logging.LoggingFeature;
 
 /**
  *
@@ -48,44 +54,147 @@ public class Toggl2RDF {
     final static DateTimeFormatter formatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
     
     final String authHeaderValue;
-    final WebTarget webTarget;
-
+    final WebTarget apiRoot;
+    final Client client;
+    
     public Toggl2RDF(final String togglApiToken) {
         authHeaderValue = authHeaderValue(togglApiToken, "api_token");
-        final Logger logger = Logger.getLogger("FLOW");
-        final Feature feature = new LoggingFeature(logger, Level.INFO, null, null);
-        final Client client = ClientBuilder.newClient(new ClientConfig().register(feature));
-        webTarget = client.target("https://www.toggl.com/api/v8");
+        //final Logger logger = Logger.getLogger("FLOW");
+        //final Feature feature = new LoggingFeature(logger, Level.INFO, null, null);
+        client = ClientBuilder.newClient(new ClientConfig()); //.register(feature));
+        apiRoot = client.target("https://www.toggl.com/api/v8");
     }
 
-    /*private Graph getProjects() {
-        final WebTarget projects = webTarget.path("projects");
-        projects.register(new JarqlMBR("CONSTRUCT WHERE {?s ?p ?o}"));
-        return invoke(projects);
-    }*/
+    class WorkspaceClient {
 
-    private Graph getWorkspacesAndProjects() {
-        final WebTarget projects = webTarget.path("workspaces");
+        final IRI workspace;
+        final WebTarget workspaceWebTarget;
+        public WorkspaceClient(final IRI workspace) {
+            this.workspace = workspace;
+            workspaceWebTarget = client.target(workspace.getUnicodeString());
+        }
+        
+        private Graph getProjects() {
+            final WebTarget projects = workspaceWebTarget.path("projects");
+            projects.register(new JarqlMBR(""
+                + "PREFIX toggl: <http://vocab.linked.solutions/toggl#>"
+                + "PREFIX jarql: <http://jarql.com/>"
+                + "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>"
+                + "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"
+                + "CONSTRUCT {"
+                + "?project a toggl:Project ;"
+                + "toggl:workspace ?workspace ;"
+                + "toggl:client ?client ;"
+                + "rdfs:label ?name."
+                + "} WHERE {"
+                + "?s a jarql:Root ;"
+                + "jarql:id ?id ;"
+                + "jarql:wid ?wid ;"
+                + "jarql:cid ?cid ;"
+                + "jarql:name ?name ."
+                + "BIND(IRI(CONCAT(\"https://toggl.com/api/v8/workspaces/\",?wid)) AS ?workspace)"
+                + "BIND(IRI(CONCAT(\"https://toggl.com/api/v8/projects/\",?id)) AS ?project)"
+                + "BIND(IRI(CONCAT(\"https://toggl.com/api/v8/clients/\",?cid)) AS ?client)"
+                + "}"));
+            return invoke(projects);
+        }
+        
+        private Graph getUsers() {
+            final WebTarget projects = workspaceWebTarget.path("users");
+            projects.register(new JarqlMBR(""
+                + "PREFIX toggl: <http://vocab.linked.solutions/toggl#>"
+                + "PREFIX jarql: <http://jarql.com/>"
+                + "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>"
+                + "PREFIX schema: <http://schema.org/>"
+                + "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"
+                + "CONSTRUCT {"
+                    + "?user a schema:Person ;"
+                    + "schema:name ?fullName ;"
+                    + "schema:email ?email ;"
+                    + "toggl:defaultWorkspace ?defaultWorkspace ."
+                + "} WHERE {"
+                    + "?s a jarql:Root ;"
+                    + "jarql:id ?id ;"
+                    + "jarql:image_url ?imageUrl ;"
+                    + "jarql:default_wid ?defaultWid ;"
+                    + "jarql:email ?email ;"
+                    + "jarql:fullname ?fullName ."
+                    + "BIND(IRI(CONCAT(\"https://toggl.com/api/v8/users/\",?id)) AS ?user)"
+                    + "BIND(IRI(CONCAT(\"https://toggl.com/api/v8/workspaces/\",?defaultWid)) AS ?defaultWorkspace)"
+                + "}"));
+            return invoke(projects);
+        }
+        
+        private Graph getClients() {
+            final WebTarget projects = workspaceWebTarget.path("clients");
+            projects.register(new JarqlMBR(""
+                + "PREFIX toggl: <http://vocab.linked.solutions/toggl#>"
+                + "PREFIX jarql: <http://jarql.com/>"
+                + "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>"
+                + "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"
+                + "CONSTRUCT {"
+                + "?client a toggl:Client ;"
+                + "toggl:workspace ?workspace ;"
+                + "rdfs:comment ?notes ;"
+                + "rdfs:label ?name."
+                + "} WHERE {"
+                + "?s a jarql:Root ;"
+                + "jarql:id ?id ;"
+                + "jarql:wid ?wid ;"
+                + "jarql:name ?name ."
+                + "OPTIONAL {$s jarql:notes ?notes }."
+                + "BIND(IRI(CONCAT(\"https://toggl.com/api/v8/workspaces/\",?wid)) AS ?workspace)"
+                + "BIND(IRI(CONCAT(\"https://toggl.com/api/v8/clients/\",?id)) AS ?client)"
+                + "}"));
+            return invoke(projects);
+        }
+        
+        /**
+         * 
+         * @return projects, users, clients but no time entries
+         */
+        private Graph getWorkspaceElements() {
+            return new UnionGraph(getProjects(), getUsers(), getClients());
+        }
+        
+    }
+    
+    
+    private Graph getElementsOfWorkspaces(Collection<IRI> workspaces) {
+        final Graph result = new UnionGraph(workspaces.stream().map(
+                i -> new WorkspaceClient(i).getWorkspaceElements()).toArray(size -> new Graph[size]));
+        return result;
+    }
+    
+    private Graph getElementsOfWorkspacesInGraph(Graph workspaceDescriptions) {
+        final Stream<Triple> workspaceTriopleIter = StreamSupport.stream(
+          Spliterators.spliteratorUnknownSize(workspaceDescriptions.filter(null, RDF.type, 
+                new IRI("http://vocab.linked.solutions/toggl#Workspace")), Spliterator.ORDERED), false);
+        return getElementsOfWorkspaces(workspaceTriopleIter.map(t -> (IRI)t.getSubject()).collect(toList()));
+    }
+
+    private Graph getWorkspaces() {
+        final WebTarget projects = apiRoot.path("workspaces");
         projects.register(new JarqlMBR(""
                 + "PREFIX toggl: <http://vocab.linked.solutions/toggl#>"
                 + "PREFIX jarql: <http://jarql.com/>"
                 + "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>"
                 + "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"
                 + "CONSTRUCT {"
-                + "?workSpace a toggl:WorkSpace;"
+                + "?workspace a toggl:Workspace;"
                 + "rdfs:label ?name."
                 + "} WHERE {"
                 + "?s a jarql:Root ;"
                 + "jarql:name ?name ;"
                 + "jarql:id ?id ."
-                + "BIND(IRI(CONCAT(\" https://www.toggl.com/api/v8/workspaces/\", ?id)) AS ?workSpace)"
+                + "BIND(IRI(CONCAT(\"https://www.toggl.com/api/v8/workspaces/\", ?id)) AS ?workspace)"
                 + "}"));
         return invoke(projects);
     }
     
     private Graph getTimeEntries(final ZonedDateTime start, final ZonedDateTime end) throws UnsupportedFormatException {
         
-        final WebTarget timeEntries = webTarget.path("time_entries");
+        final WebTarget timeEntries = apiRoot.path("time_entries");
         timeEntries.register(new JarqlMBR(""
                 + "PREFIX toggl: <http://vocab.linked.solutions/toggl#>"
                 + "PREFIX jarql: <http://jarql.com/>"
@@ -93,7 +202,7 @@ public class Toggl2RDF {
                 + "PREFIX dct <http://purl.org/dc/terms/>"
                 + "CONSTRUCT {"
                 + "?timeEntry a toggl:TimeEntry ;"
-                + "toggl:workSpace ?workSpace ;"
+                + "toggl:workspace ?workspace ;"
                 + "toggl:user ?user ;"
                 + "toggl:project ?project ;"
                 + "toggl:start ?startTime ;"
@@ -109,7 +218,7 @@ public class Toggl2RDF {
                 + "?s jarql:stop ?stop ."
                 + "OPTIONAL {?s jarql:description ?description} ."
                 + "BIND(IRI(CONCAT(\"https://toggl.com/api/v8/time_entries/\",?id)) AS ?timeEntry)"
-                + "BIND(IRI(CONCAT(\"https://toggl.com/api/v8/workspaces/\",?wid)) AS ?workSpace)"
+                + "BIND(IRI(CONCAT(\"https://toggl.com/api/v8/workspaces/\",?wid)) AS ?workspace)"
                 + "BIND(IRI(CONCAT(\"https://toggl.com/api/v8/users/\",?uid)) AS ?user)"
                 + "BIND(IRI(CONCAT(\"https://toggl.com/api/v8/projects/\",?pid)) AS ?project)"
                 + "BIND(xsd:dateTime(?start) AS ?startTime)"
@@ -137,8 +246,9 @@ public class Toggl2RDF {
         final ZonedDateTime end = ZonedDateTime.now();
         final ZonedDateTime start = end.minusDays(1);
         final Graph graph = new SimpleGraph();
-        //graph.addAll(toggl2RDF.getTimeEntries(start, end));
-        graph.addAll(toggl2RDF.getWorkspacesAndProjects());
+        graph.addAll(toggl2RDF.getTimeEntries(start, end));
+        graph.addAll(toggl2RDF.getWorkspaces());
+        graph.addAll(toggl2RDF.getElementsOfWorkspacesInGraph(graph));
         Serializer.getInstance().serialize(System.out, graph, "text/turtle");
     }
 }
